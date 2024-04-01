6433b52891c88442d7a54ffb89e53521780249be
monger18 <satyamsri1809@gmail.com>
2024-04-01 23:48:15 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     //3
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }],
     });
     if (existedUser) {
@@ -39,8 +39,16 @@ const registerUser = asyncHandler(async (req, res) => {
 
     //console.log(req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    let coverImageLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if (!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
 
     //5
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -29,7 +29,7 @@ const userSchema = new Schema(
             type: String, //cloudinary url
             required: true,
         },
-        coverimage: {
+        coverImage: {
             type: String,
         },
         watchHistory: [
@@ -65,7 +65,7 @@ userSchema.methods.generateAccessToken = function () {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullname: this.fullname,
+            fullName: this.fullName,
         },
         process.env.ACCESS_TOKEN_SCERET,
         {
