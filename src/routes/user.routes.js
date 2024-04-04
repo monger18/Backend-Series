4bb834959f46dd08d71117f5b20811040c6f6708
monger18 <satyamsri1809@gmail.com>
2024-04-04 17:11:35 +0530

    add new routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
     logoutUser,
     registerUser,
     refereshAccessToken,
+    changeCurrentUserPassword,
+    getCurrentUser,
+    updateUserDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -29,5 +37,16 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/referesh-token").post(refereshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentUserPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update").patch(updateUserDetails);
+router
+    .route("/avatar")
+    .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-Image")
+    .patch(verifyJwt, uplaod.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
